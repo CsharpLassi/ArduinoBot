7b26507fa0216fbdc161d519b6e83e040abcbcc3
Lassi <csharplassi@posteo.de>
2016-11-01 08:52:08 +0100

    Erster Bottest

--- ArduinoBot/Program.cs
+++ ArduinoBot/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Telegram.Bot;
+using System.IO;
+using System.Threading;
 
 namespace ArduinoBot
 {
@@ -6,7 +9,31 @@ namespace ArduinoBot
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tokenpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), ".develop", "bottoken");
+
+            var token = File.ReadAllText(tokenpath);
+            token = token.Substring(0, token.Length - 1);
+            TelegramBotClient bot = new TelegramBotClient(token);
+
+            CancellationTokenSource source = new CancellationTokenSource();
+
+            bot.StartReceiving(source.Token);
+            bot.OnMessage += (sender, e) =>
+            {
+                    if (e.Message.Text == "/start") 
+                    {
+                        bot.SendTextMessageAsync(e.Message.Chat.Id,string.Format("Hallo {0}",e.Message.From.FirstName));
+                    }
+                    else 
+                    {
+                        bot.SendTextMessageAsync(e.Message.Chat.Id,"Ich kann dich leider nicht verstehen");
+                    }
+            };
+
+            Console.ReadLine();
+            source.Cancel();
+
         }
     }
 }
+
